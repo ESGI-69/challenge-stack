202a6b5303ba7d23fa43ef50b0e340436d4b68ea
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-01-29 16:29:23 +0100

    feat(login-register): add login, register, security, role, and profile edition

--- src/Controller/Front/DefaultController.php
+++ src/Controller/Front/DefaultController.php
@@ -5,9 +5,11 @@ namespace App\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class DefaultController extends AbstractController
 {
+    // #[IsGranted('ROLE_VALIDATOR')]
     #[Route('/', name: 'default_index')]
     public function index(): Response
     {
