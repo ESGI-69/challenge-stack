af4f558be28391e58e23c219bd24d060e9f98058
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-01-31 22:36:12 +0100

    feat(back-user): edit user role as manager and admin

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -3,25 +3,74 @@
 namespace App\Controller\Back;
 
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\RoleType;
+use App\Form\RoleManagerType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 #[Route('/user')]
+
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_user', methods: ['GET', 'POST']) ]
+    #[IsGranted('ROLE_MANAGER')]
+     /**
+     * @param  UserRepository  $userRepository
+     * @return Response
+     */
+    #[Route('/', name: 'user_index', methods: ['GET', 'POST']) ]
     public function index(UserRepository $userRepository, Request $request): Response
     {
-        $users = $userRepository->findAll();
-        // $user = $userRepository->search($request);
-        return $this->render('back/user/index.html.twig', [
-            'users' => $users,
-            // 'user' => $user
+        if ($this->isGranted('ROLE_ADMIN')) {
+          $users = $userRepository->findAll();
+          return $this->render('back/user/index.html.twig', [
+              'users' => $users,
+          ]);
+        } elseif ($this->isGranted('ROLE_MANAGER') && !$this->isGranted('ROLE_ADMIN')){
+          $users = $userRepository->findAll();
+          foreach ($users as $key => $user) {
+            if (in_array('ROLE_ADMIN', $user->getRoles()) || in_array('ROLE_MANAGER', $user->getRoles())) {
+              unset($users[$key]);
+            }
+          }
+          return $this->render('back/user/index.html.twig', [
+              'users' => $users,
+          ]);
+        }
+    }
+
+    /**
+     * @param User $user
+     * @return Response
+     */
+    #[Route('/{id}/edit', name: 'user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+          echo "admin";
+          $form = $this->createForm(RoleType::class, $user);
+          $form->handleRequest($request);
+          if ($form->isSubmitted() && $form->isValid()) {
+              $userRepository->save($user, true);
+              return $this->redirectToRoute('admin_user_index');
+          }
+        } elseif ($this->isGranted('ROLE_MANAGER') && !$this->isGranted('ROLE_ADMIN')){
+          echo 'manager';
+          $form = $this->createForm(RoleManagerType::class, $user);
+          $form->handleRequest($request);
+          if ($form->isSubmitted() && $form->isValid()) {
+              $userRepository->save($user, true);
+              return $this->redirectToRoute('admin_user_index');
+          }
+        }
+
+        return $this->render('back/user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,7 +33,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[NotBlank]
+    // NotBlank with groups create
+    #[NotBlank(groups: ['create'])]
     #[Length(min: 6)]
     private ?string $plainPassword = null;
 
