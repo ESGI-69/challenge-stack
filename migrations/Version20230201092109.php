1e5f129a50a901a8041331b9f09a06380ed3c070
Yohan Centanni <thorbenliya@gmail.com>
2023-02-01 12:29:37 +0100

    add email activation
    Signed-off-by: Yohan Centanni <thorbenliya@gmail.com>

---
+++ migrations/Version20230201092109.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230201092109 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE "user" ADD activation_token VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE "user" ADD activation_token_expiration TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" DROP activation_token');
+        $this->addSql('ALTER TABLE "user" DROP activation_token_expiration');
+    }
+}
