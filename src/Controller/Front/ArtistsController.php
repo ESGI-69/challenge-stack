05c15da3e5d52c03456f4974a16cbab6d7273813
Yohan Centanni <thorbenliya@gmail.com>
2023-02-01 16:31:45 +0100

    get artist and top followed artist in controller
    Signed-off-by: Yohan Centanni <thorbenliya@gmail.com>

--- src/Controller/Front/ArtistsController.php
+++ src/Controller/Front/ArtistsController.php
@@ -5,14 +5,28 @@ namespace App\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Artist;
+use App\Repository\ArtistRepository;
 
 class ArtistsController extends AbstractController
 {
     #[Route('/artists', name: 'artists')]
-    public function index(): Response
+    public function index(ArtistRepository $repository): Response
     {
+        $artists = $repository->findBy([], ['id' => 'DESC'], 10);
+        $query = $repository->createQueryBuilder('a')
+        ->select('a, COUNT(au.id) as followers')
+        ->leftJoin('a.followed', 'au')
+        ->groupBy('a.id')
+        ->orderBy('followers', 'DESC')
+        ->setMaxResults(10)
+        ->getQuery();
+        $artistsWithTopFollowers = $query->getResult();
+
         return $this->render('front/artists/index.html.twig', [
             'controller_name' => 'ArtistsController',
+            'artists' => $artists,
+            'artistsWithTopFollowers' => $artistsWithTopFollowers
         ]);
     }
 }
