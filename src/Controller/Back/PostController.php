5a1395b3c72e956688fe0e5b1b772f330722ac5e
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-02-03 20:54:12 +0100

    feat(back-crud-article): edit delete and add done

--- src/Controller/Back/PostController.php
+++ src/Controller/Back/PostController.php
@@ -2,29 +2,89 @@
 
 namespace App\Controller\Back;
 
+use App\Entity\Post;
+use App\Entity\User;
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use App\Repository\PostRepository;
 
+#[Route('/post')]
 class PostController extends AbstractController
 {
-  #[IsGranted('ROLE_MANAGER')]
-  #[Route('/post-validation', name: 'validate_post')]
-  public function index(PostRepository $postRepository): Response
-  {
-    // get artist id linked to the current user
-    $artistId = $this->getUser()->getIdArtist()->getId();
-    // All unvalidated posts ID from the artist
-    $unvalidatedPostIds = $postRepository->getUnvalidatedPostIdsFromArtist($artistId);
-    // Retrive all unvalidated posts from the artist
-    foreach ($unvalidatedPostIds as $unvalidatedPostId) {
-      $unvalidatedPosts[] = $postRepository->find($unvalidatedPostId);
+    #[Route('/', name: 'app_post_index', methods: ['GET'])]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('back/post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
     }
 
-    return $this->render('Back/post/validation.html.twig', [
-        'unvalidatedPosts' => $unvalidatedPosts,
-    ]);
-  }
+    #[Route('/new', name: 'app_post_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, PostRepository $postRepository): Response
+    {   
+        $linkedArtist = true;
+        $idArtist = $this->getUser()->getIdArtist();
+        if ($idArtist === null) {
+          $linkedArtist = false;
+        }
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // set post created_at date now (DateTimeImmutable)
+            $post->setCreatedAt(new \DateTimeImmutable());
+            $post->setUpdatedAt(new \DateTimeImmutable());
+            $post->setIdUser($this->getUser());
+            $post->setIdArtist($idArtist);
+            $postRepository->save($post, true);
+            return $this->redirectToRoute('admin_app_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/post/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+            'linkedArtist' => $linkedArtist,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('back/post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_post_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, PostRepository $postRepository): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setUpdatedAt(new \DateTimeImmutable());
+            $postRepository->save($post, true);
+
+            return $this->redirectToRoute('admin_app_post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_post_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, PostRepository $postRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $postRepository->remove($post, true);
+        }
+
+        return $this->redirectToRoute('admin_app_post_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
