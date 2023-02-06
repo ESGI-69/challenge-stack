8cc0d70f86e323bf59c1ddb1743d001bbe914122
mrpinkcat <gatien.anizan@gmail.com>
2023-02-06 12:49:17 +0100

    feat(commentValidation): add comment validation

--- src/Controller/Back/CommentController.php
+++ src/Controller/Back/CommentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Back;
 
+use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -11,7 +13,7 @@ use App\Repository\CommentRepository;
 class CommentController extends AbstractController
 {
   #[IsGranted('ROLE_CAN_MODERATE_COMMENT')]
-  #[Route('/comment-validation', name: 'comment_validation')]
+  #[Route('/comment-validation', name: 'app_comment_validation')]
   public function index(CommentRepository $commentRepository): Response
   {
     $unvalidatedComment = $commentRepository->findBy(['validated_at' => null]);
@@ -19,5 +21,25 @@ class CommentController extends AbstractController
     return $this->render('Back/comment/validation.html.twig', [
       'unvalidatedComment' => $unvalidatedComment,
     ]);
+  }
+
+  #[IsGranted('ROLE_CAN_MODERATE_COMMENT')]
+  #[Route('/{id}/validate', name: 'app_comment_validate', methods: ['GET'])]
+  public function validate(Comment $comment, CommentRepository $commentRepository): Response
+  {
+    $comment->setValidatedAt(new \DateTimeImmutable());
+    $commentRepository->save($comment, true);
+    return $this->redirectToRoute('admin_app_comment_validation', [], Response::HTTP_SEE_OTHER);
+  }
+
+  #[IsGranted('ROLE_CAN_MODERATE_COMMENT')]
+  #[Route('/{id}/del-validation', name: 'app_comment_delete_validation', methods: ['POST'])]
+  public function deleteValidation(Request $request, Comment $comment, CommentRepository $commentRepository): Response
+  {
+      if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
+          $commentRepository->remove($comment, true);
+      }
+
+      return $this->redirectToRoute('admin_app_comment_validation', [], Response::HTTP_SEE_OTHER);
   }
 }
