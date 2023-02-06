fb5c110069933339ad01ee3cb4d6b665ea181d4d
mrpinkcat <gatien.anizan@gmail.com>
2023-02-06 20:04:44 +0100

    feat(comments): change the comment count algo & add a message when no comment have been posted

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -41,7 +41,7 @@ class PostRepository extends ServiceEntityRepository
 
     public function getFollowedArtistPosts($user, $count = 10)
     {
-        $dirtyResult = $this->createQueryBuilder('post')
+        $result = $this->createQueryBuilder('post')
         ->leftJoin('post.id_artist', 'postArtist')
         ->leftJoin('post.comments', 'postComments')
         ->leftJoin('postComments.id_user', 'postCommentsUser')
@@ -54,28 +54,12 @@ class PostRepository extends ServiceEntityRepository
         ->addSelect('postCommentsUser')
         ->addSelect('postArtistFollowed')
         ->groupBy('post.id', 'postArtist.id', 'postComments.id', 'postLikes.id', 'postCommentsUser.id', 'postArtistFollowed.id')
-        ->addSelect('COUNT(postComments.id) as commentCount')
-        ->addSelect('COUNT(postLikes.id) as likeCount')
         ->orderBy('post.created_at', 'DESC')
         ->setMaxResults($count)
         ->getQuery()
         ->getResult();
 
-        $result = [];
-        foreach ($dirtyResult as $post) {
-            if ($post[0]->getIdArtist()->getFollowed()->contains($user)) {
-                $result[] = $post;
-            }
-        }
-        
-        $resultEnhance = [];
-        foreach ($result as $row) {
-            $row[0]->likeCount = $row['likeCount'];
-            $row[0]->commentCount = $row['commentCount'];
-            $resultEnhance[] = $row[0];
-        }
-
-        return $resultEnhance;
+        return $result;
     }
 
     public function getLastCreate(int $count = 5): array
