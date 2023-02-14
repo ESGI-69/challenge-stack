35e9cb5512c3cbbabb60011883325062802f6f14
Quentin Peltier <quentin.peltier.pro@gmail.com>
2023-02-14 12:23:04 +0100

    feat(124-add-likes-to-posts): like and unlike done

--- src/Controller/Front/PostController.php
+++ src/Controller/Front/PostController.php
@@ -22,7 +22,7 @@ class PostController extends AbstractController
     $comment = new Comment();
     $form = $this->createForm(CommentType::class, $comment);
     $form->handleRequest($request);
-
+    
     if ($form->isSubmitted() && $form->isValid()) {
       $comment->setIdUser($this->getUser());
       $comment->setIdPost($post);
@@ -30,14 +30,38 @@ class PostController extends AbstractController
       $comment->setValidatedAt(null);
       
       $commentRepository->save($comment, true);
-
       return $this->redirectToRoute('front_app_post_show', ["slug" => $post->getSlug()], Response::HTTP_SEE_OTHER);
     }
 
+    $postLiked = $post->getUserslike()->contains($this->getUser());
+
     return $this->render('Front/post/show.html.twig', [
       'post' => $post,
-        'form' => $form->createView(),
+      'form' => $form->createView(),
+      'postLiked' => $postLiked
+
     ]);
   }
+
+  #[Route('/{id}/like', name: 'app_post_like', methods: ['GET'])]
+  public function like(Post $post, PostRepository $postRepository, Request $request): Response
+  {
+    $referer = $request->headers->get('referer');
+    $post->addUserslike($this->getUser());
+    $postRepository->save($post, true);
+
+    return $this->redirect($referer);
+  }
+
+  #[Route('/{id}/unlike', name: 'app_post_unlike', methods: ['GET'])]
+  public function unlike(Post $post, PostRepository $postRepository, Request $request): Response
+  {
+    $referer = $request->headers->get('referer');
+    $post->removeUserslike($this->getUser());
+    $postRepository->save($post, true);
+
+    return $this->redirect($referer);
+  }
+
   
 }
