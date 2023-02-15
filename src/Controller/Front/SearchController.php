51ca0b4103586113e396d964c930a48bdd1937ed
Yohan Centanni <thorbenliya@gmail.com>
2023-02-16 00:14:50 +0100

    search bar works with manual form
    Signed-off-by: Yohan Centanni <thorbenliya@gmail.com>

--- src/Controller/Front/SearchController.php
+++ src/Controller/Front/SearchController.php
@@ -11,7 +11,7 @@ use App\Entity\Artist;
 use App\Entity\ConcertHall;
 use app\Entity\Event;
 use App\Form\SearchType;
-
+use Symfony\Component\Security\Csrf\Exception\InvalidCsrfTokenException;
 
 class SearchController extends AbstractController
 {
@@ -21,34 +21,45 @@ class SearchController extends AbstractController
         
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $search = $form->getData();
-            $search = $search['search'];
-
-            // search in the database for artist name
-            $artistRepository = $doctrine->getRepository(Artist::class);
-            $artists = $artistRepository->findBy(['pseudo' => $search]);
-            // search in the database for concert hall name
-            $concertHallRepository = $doctrine->getRepository(ConcertHall::class);
-            $concertHalls = $concertHallRepository->findBy(['name' => $search]);
-            // search in the database for event name
-            $eventRepository = $doctrine->getRepository(Event::class);
-            $events = $eventRepository->findBy(['title' => $search]);
-
-            
-            return $this->render('Front/search/index.html.twig', [
-                'search' => $search,
-                'artists' => $artists,
-                'concertHalls' => $concertHalls,
-                'events' => $events,
-                'form' => $form->createView()
-            ]);
-        }
         
-        return $this->render('Front/search/index.html.twig', [
-            'form' => $form->createView()
-        ]);
+                if (($form->isSubmitted() && $form->isValid())) {
+
+                    $search = $form->getData();
+                    $search = $search['search'];
+                
+                }else{
+                    if ($request->isMethod('POST')) { 
+                        
+                        if ($this->isCsrfTokenValid('search', $request->request->get('_token'))) {
+                            $search = $request->request->get('search') ;
+                            $search = htmlspecialchars($search, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+                        }else{
+                            throw new \Exception('CSRF token is invalid');
+                        }                       
+                    }else{
+                        return $this->render('Front/search/index.html.twig', [
+                            'form' => $form->createView()
+                        ]);
+                    }
+                }
+
+                  $artistRepository = $doctrine->getRepository(Artist::class);
+                  $artists = $artistRepository->findBy(['pseudo' => $search]);
+
+                  $concertHallRepository = $doctrine->getRepository(ConcertHall::class);
+                  $concertHalls = $concertHallRepository->findBy(['name' => $search]);
+
+                  $eventRepository = $doctrine->getRepository(Event::class);
+                  $events = $eventRepository->findBy(['title' => $search]);
+                  
+                  return $this->render('Front/search/index.html.twig', [
+                      'search' => $search,
+                      'artists' => $artists,
+                      'concertHalls' => $concertHalls,
+                      'events' => $events,
+                      'form' => $form->createView()
+                  ]);
+
 
     }
 }
